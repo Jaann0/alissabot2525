590ed9f96c074a7d771a73971115438313967b09
HakiBl4ck <53626680+HakiBl4ck@users.noreply.github.com>
2022-06-08 05:56:18 +0000

    Youtube MP3 Music cmd added

--- alissabot.py
+++ alissabot.py
@@ -69,8 +69,24 @@ async def on_abrazo(ctx: Context):
 
 @bot.command("cancion")
 async def on_song(ctx: Context):
-	adio = await ctx.download_from_link("https://cdn.pixabay.com/download/audio/2022/06/04/audio_11d4323329.mp3?filename=the-last-piano-112677.mp3")
-	await ctx.send_audio(adio)
+	mulan = ctx.msg.content[27:37]
+	url = "https://youtube-mp3-download1.p.rapidapi.com/dl"
+	querystring = {"id":mulan}
+	headers = {
+		"X-RapidAPI-Host": "youtube-mp3-download1.p.rapidapi.com",
+		"X-RapidAPI-Key": "082395124cmsh8f011e89c74584fp1b5c87jsn52b9ca1173b4"
+}
+	response = requests.get(url, headers=headers, params=querystring)
+	data = response.json()
+	url = data["link"]
+	titulo = data["title"]
+	duracion = data["duration"]
+	if duracion <= 180:
+		adio = await ctx.download_from_link(url)
+		await ctx.send_audio(adio)
+		await ctx.send("►Cancion: " + titulo + "\nSolicitada por: " + ctx.msg.author.nickname)
+	else:
+		await ctx.send("La cancion excede la duracion de audio en amino (3min)")
 
 @bot.command("play")
 async def on_video(ctx: Context):
