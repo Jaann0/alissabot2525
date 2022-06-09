fdd7da739764d3c2eb1baeb9c5070f283530a22f
HakiBl4ck <53626680+HakiBl4ck@users.noreply.github.com>
2022-06-09 15:47:02 -0600

    getid fixed

--- alissabot.py
+++ alissabot.py
@@ -90,6 +90,6 @@ async def on_video(ctx: Context):
 async def on_getid(ctx: Context):
 	delta = ctx.msg.content[8:40]
 	alubia = await ctx.client.get_info_link(delta)
-	await ctx.send(str(alubia))
+	await ctx.send(str(alubia[43:79]))
 
 bot.start()
