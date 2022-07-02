199dac46e20f453f6827dfa37307907d62b6d2e5
HakiBl4ck <53626680+HakiBl4ck@users.noreply.github.com>
2022-07-02 02:06:44 -0600

    Update alissabot.py

--- alissabot.py
+++ alissabot.py
@@ -97,7 +97,7 @@ async def on_video(ctx: Context):
 async def on_getid(ctx: Context):
 	delta = ctx.msg.content[7:40]
 	alubia = await ctx.client.get_info_link(delta)
-	await ctx.send(str(alubia["linkInfo"]))
+	await ctx.send(str(alubia.linkInfo.objectId) + "\n" + str(alubia.linkInfo.shareURLFullPath))
 	
 @bot.command("play")
 async def on_musica(ctx: Context):
