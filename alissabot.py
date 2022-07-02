00a12ccc3d83f37de52df1b4b3a7189eafb9adeb
HakiBl4ck <53626680+HakiBl4ck@users.noreply.github.com>
2022-07-02 16:48:50 -0600

    Update alissabot.py

--- alissabot.py
+++ alissabot.py
@@ -99,7 +99,7 @@ async def on_getid(ctx: Context):
 	delta = ctx.msg.content[7:40]
 	alubia = await ctx.get_info_link(delta)
 	profi = str(alubia.linkInfo.objectId)
-	await ctx.send(str(UserProfile(uid=profi).nickname))
+	await ctx.send(str(UserProfile(uid=profi).aminoId))
 	
 @bot.command("play")
 async def on_musica(ctx: Context):
