e70c676d6fa2602902c1a10d3ab7a1d512bfa87b
HakiBl4ck <53626680+HakiBl4ck@users.noreply.github.com>
2022-07-05 15:12:54 -0600

    Update alissabot.py

--- alissabot.py
+++ alissabot.py
@@ -165,7 +165,7 @@ async def on_casados(ctx: Context):
 	await ctx.send(novio + ", se quiere casar contigo " + polola + " tu aceptas? (Escribe acepto si asi lo deseas)")
 	def siono(m: Message):
 		return m.o.chatMessage.content == 'acepto'
-	marry = await bot.wait_for(check=siono)
+	marry = await bot.wait_for(check=siono, 2.5)
 	novia = marry.o.chatMessage.author.nickname
 	kunno = sqlite3.connect("matriz.db")
 	curzor = kunno.cursor()
