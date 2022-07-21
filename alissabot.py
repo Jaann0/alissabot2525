f1d11e8db8d9fefff52f353c4b338b03ec9e9730
HakiBl4ck <53626680+HakiBl4ck@users.noreply.github.com>
2022-07-21 16:37:39 -0600

    Update alissabot.py

--- alissabot.py
+++ alissabot.py
@@ -110,7 +110,7 @@ async def on_getid(ctx: Context):
 	delta = ctx.msg.content[7:40]
 	alubia = await ctx.get_info_link(delta)
 	oid = alubia.linkInfo.objectId
-	tuba = await ctx.client.get_link_identify(oid)
+	tuba = await ctx.client.get_link_identify(code=oid)
 	await ctx.send(str(tuba))
 	
 @bot.command("play")
